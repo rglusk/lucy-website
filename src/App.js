051d0f57b7b68a8c6f0c5ec19fa6bdf48f8d5fe5
Riley Glusker <riley.glusker@gmail.com>
2017-06-29 19:10:07 +0100

    Made minor change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Rileyland</h2>
+          <h2>Welcome to Rileyland changing to test git</h2>
         </div>
         <p className="App-intro">
           test test
